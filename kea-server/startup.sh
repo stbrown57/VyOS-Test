cf54ef46f97e694aa2c0aa1a8693a9891a940a86
Stephen Brown <stbrown@6browns.org>
2025-08-29 17:40:46 -0400

    added kea

---
+++ kea-server/startup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Enable IP forwarding
+sysctl -w net.ipv4.ip_forward=1
+
+# Manually assign the static IP to the eth1 interface
+ip addr add 192.168.100.1/24 dev eth1
+
+# Set the default gateway
+ip route add default via 10.89.0.1
+
+# Start the Kea services
+kea-dhcp4 -c /etc/kea/kea-dhcp4.conf &
+kea-ctrl-agent -c /etc/kea/kea-ctrl-agent.conf &
+wait
